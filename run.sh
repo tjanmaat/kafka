3c31f03b580c95a342765fafd2469242b035c3d1
tomja <t.janmaat@landlifecomapny.com>
2021-07-23 19:18:31 +0200

    Add bash script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# build container
+docker build . -f Dockerfile_kafka -t kafka:latest
+# serve containers
+docker-compose up -d
+
+# do not close terminal right away
+read
+
+docker-compose down
