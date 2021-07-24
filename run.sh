f001ffbf34be5673c5b311b27a527e7665fc71ea
tomja <t.janmaat@landlifecomapny.com>
2021-07-24 14:29:00 +0200

    Rename dockerfile retag docker image

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # build container
-docker build . -f Dockerfile_kafka -t kafka:latest
+docker build . -f Dockerfile_producer -t producer:latest
 # serve containers
 docker-compose up -d
 
