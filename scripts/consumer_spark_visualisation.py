33f4f7036af82a9755e8212311da20f6dc0f7928
tomja <t.janmaat@landlifecomapny.com>
2021-07-25 19:07:52 +0200

    Fix schema of spark df, write into console

--- scripts/consumer_spark_visualisation.py
+++ scripts/consumer_spark_visualisation.py
@@ -1,8 +1,10 @@
+from time import sleep
 
-from pyspark.sql.functions import window, col, current_timestamp, to_json, struct
+from pyspark.sql.types import StringType, StructType, StructField, IntegerType
+from pyspark.sql.functions import window, col, current_timestamp, to_json, struct, from_json, to_timestamp, \
+    from_unixtime
 from common import get_stream_dataframe_from_kafka_topic, get_spark_session
 
-from time import sleep
 import seaborn as sns
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,41 +12,24 @@ import matplotlib.pyplot as plt
 
 def create_event_schema():
     return StructType(
-        [StructField("$schema", StringType(), True),
-         StructField("bot", BooleanType(), True),
-         StructField("comment", StringType(), True),
-         StructField("id", StringType(), True),
-         StructField("length",
-                     StructType(
-                         [StructField("new", IntegerType(), True),
-                          StructField("old", IntegerType(), True)]), True),
-         StructField("meta",
-                     StructType(
-                         [StructField("domain", StringType(), True),
-                          StructField("dt", StringType(), True),
-                          StructField("id", StringType(), True),
-                          StructField("offset", LongType(), True),
-                          StructField("partition", LongType(), True),
-                          StructField("request_id", StringType(), True),
-                          StructField("stream", StringType(), True),
-                          StructField("topic", StringType(), True),
-                          StructField("uri", StringType(), True)]), True),
-         StructField("minor", BooleanType(), True),
-         StructField("namespace", IntegerType(), True),
-         StructField("parsedcomment", StringType(), True),
-         StructField("patrolled", BooleanType(), True),
-         StructField("revision",
-                     StructType(
-                         [StructField("new", IntegerType(), True),
-                          StructField("old", IntegerType(), True)]), True),
-         StructField("server_name", StringType(), True),
-         StructField("server_script_path", StringType(), True),
-         StructField("server_url", StringType(), True),
-         StructField("timestamp", StringType(), True),
-         StructField("title", StringType(), True),
-         StructField("type", StringType(), True),
-         StructField("user", StringType(), True),
-         StructField("wiki", StringType(), True)])
+        [StructField("count", IntegerType(), True),
+         StructField("user", StringType(), True)])
+
+
+def format_stream_dataframe(dataframe):
+    # Event data schema
+    schema_wiki = create_event_schema()
+
+    # Create dataframe setting schema for event data
+    df_wiki = (dataframe
+               # Sets schema for event data
+               .withColumn("value", from_json("value", schema_wiki))
+               )
+
+    # Transform into tabular
+    # Select relevant columns
+    return (df_wiki.select("value.count"
+                           , "value.user"))
 
 
 def create_querystream(dataframe):
@@ -55,91 +40,6 @@ def create_querystream(dataframe):
             .queryName("wiki_changes")
             .outputMode("update")
             .start())
-
-
-def visualize_stream(queryStream, spark):
-
-    matplotlib.rc('font', family='DejaVu Sans')
-    sns.set(style="whitegrid")
-
-    print("**********************")
-    print("General Info")
-    print("**********************")
-    print("Run:{}".format(i))
-    if len(queryStream.recentProgress) > 0:
-        print("Stream timestamp:{}".format(queryStream.lastProgress["timestamp"]))
-        print("Watermark:{}".format(queryStream.lastProgress["eventTime"]["watermark"]))
-        print("Total Rows:{}".format(queryStream.lastProgress["stateOperators"][0]["numRowsTotal"]))
-        print("Updated Rows:{}".format(queryStream.lastProgress["stateOperators"][0]["numRowsUpdated"]))
-        print("Memory used MB:{}".format(
-            (queryStream.lastProgress["stateOperators"][0]["memoryUsedBytes"]) * 0.000001))
-
-    df = spark.sql(
-        """
-            select
-                window.start
-                ,window.end
-                ,user
-                ,sum(count) count
-            from
-                wiki_changes
-            where
-                window.start = (select max(window.start) from wiki_changes)
-            group by
-                window.start
-                ,window.end
-                ,user
-            order by
-                4 desc
-            limit 10
-        """
-    ).toPandas()
-
-    # Plot the total crashes
-    sns.set_color_codes("muted")
-
-    # Initialize the matplotlib figure
-    plt.figure(figsize=(8, 6))
-
-    print("**********************")
-    print("Graph - Top 10 users")
-    print("**********************")
-    try:
-        # Barplot
-        sns.barplot(x="count", y="user", data=df)
-
-        # Show barplot
-        plt.show()
-    except ValueError:
-        # If Dataframe is empty, pass
-        pass
-
-    print("**********************")
-    print("Table - Top 10 users")
-    print("**********************")
-    print(df)
-
-    print("**********************")
-    print("Table - Count by aggregation window")
-    print("**********************")
-    df1 = spark.sql(
-        """
-            select
-                window.start
-                ,window.end
-                ,sum(count) qty_lines
-                ,count(distinct user) qty_users
-            from
-                wiki_changes
-            group by
-                window.start
-                ,window.end
-            order by
-                window.start desc
-        """
-    ).toPandas()
-
-    print(df1)
 
 
 if __name__ == '__main__':
@@ -154,24 +54,12 @@ if __name__ == '__main__':
     stream_dataframe = get_stream_dataframe_from_kafka_topic(kafka_server,
                                                              "wiki-formatted",
                                                              spark)
+    # Format stream dataframe
+    df_formatted = format_stream_dataframe(stream_dataframe)
 
-    query = (stream_dataframe.select(to_json(struct("*")).alias("value"))
-             .selectExpr("CAST(value AS STRING)")
+    query = (df_formatted
              .writeStream
              .format("console")
              .start())
 
     query.awaitTermination()
-
-    # # Create query stream with memory sink
-    # queryStream = create_querystream(stream_dataframe)
-    #
-    # try:
-    #     i = 1
-    #     while True:
-    #         visualize_stream(queryStream, spark)
-    #
-    #         sleep(10)
-    #         i = i + 1
-    # except KeyboardInterrupt:
-    #     print("process interrupted.")
